ca8eb49b0ee9ffe26c7e622214578371cadea0a0
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-03-15 00:17:27 +0000

    Document

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-from collections import deque
+"""Defines a function that determines if a box containing a list
+   of lists can be opened using keys stored in the lists
+"""
 
 
 def canUnlockAll(boxes):
