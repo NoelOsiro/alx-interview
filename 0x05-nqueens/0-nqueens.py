e282447d7d0d6bdf2663417db53677954d6c6c4c
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-04-12 01:04:35 +0300

    Create 0-nqueens.py

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+import sys
+
+def is_safe(board, row, col):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    
+    return True
+
+def solve_nqueens(board, col):
+    if col >= len(board):
+        print_solution(board)
+        return True
+    
+    res = False
+    for i in range(len(board)):
+        if is_safe(board, i, col):
+            board[i][col] = 1
+            res = solve_nqueens(board, col + 1) or res
+            board[i][col] = 0
+    
+    return res
+
+def print_solution(board):
+    for row in board:
+        print(row)
+    print()
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    
+    try:
+        n = int(sys.argv[1])
+        if n < 4:
+            print("N must be at least 4")
+            sys.exit(1)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    if not solve_nqueens(board, 0):
+        print("No solution found.")
+
+if __name__ == "__main__":
+    main()
