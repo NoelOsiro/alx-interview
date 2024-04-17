bf0a4444a3decb42dccf072c7830412e50faac93
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-04-17 16:02:22 +0000

    bobba fett

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,42 +1,23 @@
 #!/usr/bin/node
+
 import request from 'request';
 
-const movieId = process.argv[2];
+const id = process.argv[2] + '/';
+const url = 'https://swapi-api.hbtn.io/api/films/';
 
-if (!movieId) {
-  console.error('Usage: node script.js <Movie ID>');
-  process.exit(1);
-}
+request(url + id, async function (err, res, body) {
+  if (err) return console.error(err);
 
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+  const listUrl = JSON.parse(body).characters;
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    process.exit(1);
-  }
+  for (const li of listUrl) {
+    await new Promise(function (resolve, reject) {
+      request(li, function (err, res, body) {
+        if (err) return console.error(err);
 
-  if (response.statusCode !== 200) {
-    console.error('Unexpected status code:', response.statusCode);
-    process.exit(1);
-  }
-
-  const filmData = JSON.parse(body);
-  if (!filmData.characters || !filmData.characters.length) {
-    console.error('No characters found for this movie.');
-    process.exit(1);
-  }
-
-  filmData.characters.forEach((characterUrl) => {
-    request(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error fetching character:', charError);
-      } else if (charResponse.statusCode !== 200) {
-        console.error('Unexpected status code for character:', charResponse.statusCode);
-      } else {
-        const characterData = JSON.parse(charBody);
-        console.log(characterData.name);
-      }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
     });
-  });
+  }
 });
